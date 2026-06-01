7c9a046fee722e8dbc0f5b4e7871eab51e08b7a7
Oliver Henwood <henwoodo97@gmail.com>
2026-06-01 15:20:42 +0100

    OKAY THIS FINALLY WORKS IN WEBPACK SO REMAIN CALM AND CARRY ON CODING

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 // webpack.config.js
 import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
   mode: "development",
   entry: "./src/index.js",
+  devServer: {
+    static: "./dist",
+  },
   output: {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
